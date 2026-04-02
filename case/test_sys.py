65128089a83113dc38c3adefb7985127341711ba
slayfune <slayfune@yeah.net>
2026-04-02 08:45:12 +0800

    用例优化

--- case/test_sys.py
+++ case/test_sys.py
@@ -3,6 +3,7 @@ from function.read_file import control_file
 from function.assert_respons import assert_response
 import allure
 import pytest
+import requests
 
 
 con_file = control_file(sheet_name='用例')
@@ -14,6 +15,11 @@ case_data = con_file.read_excel()
 class Test_sys:
 
     @allure.title('测试添加敏感词配置的用例')
+    @pytest.mark.flaky(reruns=5, reruns_delay=1, only_rerun=[requests.Timeout, requests.ConnectionError])
+    # - reruns: 失败后的重试次数
+    # - reruns_delay: 每次重试之间的延迟（秒）
+    # - only_rerun参数：仅在指定异常发生时重试,其他异常直接失败不重试
+    @pytest.mark.order(1)
     def test_add_sysconf(self,get_token):
         path = case_data[1][1]
         request_data = con_file.str_to_dict(case_data[1][3])
@@ -22,6 +28,7 @@ class Test_sys:
 
     @allure.title('测试添加小屏看板的用例')
     @pytest.mark.dependency(name = 'test_add_sysconf')
+    @pytest.mark.order(2)
     def test_xiaoping(self):
         path = case_data[0][1]
         request_data = con_file.str_to_dict(case_data[0][3])
