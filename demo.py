5fbe645d9c51b7f235274f6338ca195f9b18676d
slayfune <slayfune@yeah.net>
2026-07-22 16:53:40 +0800

    vscode推送

--- demo.py
+++ demo.py
@@ -1,8 +1,4 @@
-from function.read_file import control_file
+def trytest():
+    print('hello_world')
 
-con_file = control_file('不动产')
-
-con_file.update_excel(3,5,json_key='housingType',json_value='8')
-
-con_file.read_excel()
-
+trytest()
